06015f158b32209dc12346f61a5c0c1b945c3261
hchsu <hchsu888@gmail.com>
2018-01-20 14:48:24 +0800

    On branch master  Your branch is up-to-date with 'origin/master'.  Changes to be committed: 	modified:   RGF.py
    Added Hamilatonian_normal_metal()
    Added diagonlize_Hamiltonian()

--- RGF.py
+++ RGF.py
@@ -1,1 +1,15 @@
 ##Thiis is to practice git and RGF 
+import numpy as np
+from matplotlitb import pyplot as plt
+
+def Hamiltonian_normal_metal ( m, B, nx, ny, boundary_condition = 'PBC' ):
+	H = B*( np.eye(nx*ny, k=1) + np.eye(nx*ny, k=-1) ) + m* np.eye(nx*ny, k=0)	
+	if( boundary_condition == 'PBC'):
+		H[nx*ny-1,0]= B
+		H[0, nx*ny -1]=B	
+	return H
+
+def diagonalize_Hamiltonian(H):
+	eigen_values, eigen_vect = np.linalg.eig(H)
+	return eigen_values, eigen_vect	
+
